e341224acf33a0e65c946c1118d68783ee00af07
luis.miguel.santos.serrano <luis.miguel.santos.serrano@0939aae4-2d5c-11df-bba7-995ec2794b1c>
2010-07-07 23:52:50 +0000

    Added two new examples to test the code on. The example under package examples.nested_function2 shows that there is yet another bug in the code. Also, for example examples.nested_constructor , there is a bug in the code if the fill() method is called directly by the static main, instead of inside the box's constructor. Also, the examples package works, with the Thread example with the Gluttons, but this one shows that there are some eventual bugs with the drawing classes that have yet to be fixed. In any case, the major flaw the aspect had, for nested constructors has been fixed. The comments and system.out.prints for debug have been left as is, for they will be yet useful to fix the other issues and bugs already noticed. The around() from the aspect related to the functions should only be uncommented after those bugs have been fixed for the method's around(), since both will most likely be the same. Due to time issues, I do not know when next update will be possible to fix those other bugs that have been spotted.

---
+++ src/examples/nested_function/Main3.java
@@ -0,0 +1,13 @@
+package examples.nested_function;
+
+public class Main3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FunctionsBox f = new FunctionsBox();
+		f.callAllFunctions();
+	}
+
+}
---
+++ src/examples/nested_function2/D.java
@@ -0,0 +1,14 @@
+package examples.nested_function2;
+
+public class D {
+
+	public D() {
+		//someMethodDPrivate();
+	}
+	
+	public void someMethodD() {
+		
+	}
+	
+	
+}

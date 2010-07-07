7a7709c6e2fee16228389dd6e41c0f53d75287ad
luis.miguel.santos.serrano <luis.miguel.santos.serrano@0939aae4-2d5c-11df-bba7-995ec2794b1c>
2010-07-07 23:03:05 +0000

    Edited the examples for further testing. Fixed the bug with the aspect which would lead to wrong diagram representations when there were functions called upon object construction and upon constructions of other objects inside an object's constructor.

--- src/examples/nested_constructor/Item.java
+++ src/examples/nested_constructor/Item.java
@@ -3,9 +3,17 @@ package examples.nested_constructor;
 public class Item {
 
 	private String name;
+	private Label label1;
+	private Label label2;
 	
 	public Item(String name) {
 		this.name = name;
+		fillLabel();
 	}
 
+	private void fillLabel() {
+		label1 = new Label("Label 1");
+		label2 = new Label("Label 2");
+	}
+	
 }
---
+++ src/examples/nested_constructor/Label.java
@@ -0,0 +1,9 @@
+package examples.nested_constructor;
+
+public class Label {
+	private String name;
+	
+	public Label(String name) {
+		this.name = name;
+	}
+}
